a390161e4cfcbd305feff063743dab2710d33544
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-11-06 17:10:30 +0100

    two

--- print gray code/main.py
+++ print gray code/main.py
@@ -2,6 +2,12 @@
 def main():
     n=input("entrer la puissance de 2^n a evaluer")
     n=int(n)
+    while(n<1 or n>16):
+        print("la puissance doit etre comprise entre 1 et 16")
+        n=input("entrer la puissance de 2^n a evaluer")
+        n=int(n)
+    
+
     
     for i in range(2**n): 
         dec=i
